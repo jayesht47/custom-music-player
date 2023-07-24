0db3383271378a3f9d783585250e7dfc870fd502
Jayesh Thakur <jayeshthakur47@gmail.com>
2023-07-24 18:00:19 +0530

    updated progress bar to run based on state

--- src/components/progress-bar/progress-bar.js
+++ src/components/progress-bar/progress-bar.js
@@ -1,33 +1,36 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 const ProgressBar = (props) => {
-  const [prog, setProg] = useState(1);
-  const ref = useRef(null);
+  const [prog, setProg] = useState(0);
+  const childRef = useRef(null);
 
-  useLayoutEffect(() => {
-    console.log(ref);
-  }, []);
+  useEffect(() => {
+    updateProgressBar(prog);
+  }, [prog]);
 
   let classes = `bg-orange-600 h-1 w-0 rounded-lg`;
 
+
+  const updateProgressBar = (currentValue) => {
+    const parentOffsetWidth = childRef.current.parentElement.offsetWidth;
+    const widthPx = (currentValue * parentOffsetWidth) + "px";
+    if (childRef && childRef.current) {
+      childRef.current.style.width = widthPx;
+    }
+  }
+
   const seekHandler = (event) => {
     const parentOffsetWidth = event.target.parentElement.offsetWidth;
-    const widthPx =
-      ((event.clientX - event.target.offsetLeft) / parentOffsetWidth) *
-        parentOffsetWidth +
-      "px";
-
-    if (event.target.classList.contains(`seek-bg`))
-      event.target.children[0].style.width = widthPx;
-    else event.target.style.width = widthPx;
+    setProg((event.clientX - event.target.offsetLeft) / parentOffsetWidth);
   };
+
 
   return (
     <div
       onClick={seekHandler}
       className="seek-bg bg-gray-400 h-1 w-full hover:cursor-pointer rounded-lg"
     >
-      <div className={classes}></div>
+      <div ref={childRef} className={classes}></div>
     </div>
   );
 };
